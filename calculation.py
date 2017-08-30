25ad5a0c2d23a185379bbfe1c70d70ec069a1070
Logfert <roma.sypko@gmail.com>
2017-08-30 16:46:38 +0300

    Redone version 2.0

--- calculation.py
+++ calculation.py
@@ -37,24 +37,26 @@ def sub(x, y):
 def div(x, y):
     return x/y
 
-int_argv1 = int(sys.argv[2])
-int_argv2 = int(sys.argv[3])
 
-if (0 <= int_argv1 <= 10) and (0 <= int_argv2 <= 10):
-    if sys.argv[1] == '-add':
-        print add(int_argv1, int_argv2)
-    elif sys.argv[1] == '-mul':
-        print mul(int_argv1, int_argv2)
-    elif sys.argv[1] == '-sub':
-        print sub(int_argv1, int_argv2)
-    elif sys.argv[1] == '-div':
-        if int_argv2 != 0:
-            print div(int_argv1, int_argv2)
+def command_line(first_arg, second_arg, third_arg):
+    second_arg = int(second_arg)
+    third_arg = int(third_arg)
+
+    if (0 <= second_arg <= 10) and (0 <= third_arg <= 10):
+        if first_arg == '-add':
+            return add(second_arg, third_arg)
+        elif first_arg == '-mul':
+            return mul(second_arg, third_arg)
+        elif first_arg == '-sub':
+            return sub(second_arg, third_arg)
+        elif first_arg == '-div':
+            if third_arg != 0:
+                return div(second_arg, third_arg)
+            else:
+             return 'Division by zero.\nEnter the value of the second argument from 1 to 10.'
         else:
-            print 'Division by zero.\nEnter the value of the second argument from 1 to 10.'
+            return 'Invalid function name.\nPossible Functions: -add, -mul, -sub, -div.'
     else:
-        print 'Invalid function name.\nPossible Functions: -add, -mul, -sub, -div.'
-else:
-    print 'Invalid parameter value(s).\nPossible parameter value(s) from 0 to 10.'
+        return 'Invalid parameter value(s).\nPossible parameter value(s) from 0 to 10.'
 
-
+print command_line(sys.argv[1], sys.argv[2], sys.argv[3])
