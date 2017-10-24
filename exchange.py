b10272e956b940d5acf027395ebad30ef9934032
Logfert <roma.sypko@gmail.com>
2017-10-24 18:32:49 +0300

    New files

--- exchange.py
+++ exchange.py
@@ -5,9 +5,9 @@ import functions
 
 
 def handler():
-    list1 = functions.return_request(commons.PB_URL)
+    parsed_string = functions.return_request(commons.PB_URL)
     try:
-        for item in list1:
+        for item in parsed_string:
             if item['ccy'] == selection():
                 clear_insert([item['buy'], item['sale']])
     except Exception:
@@ -35,25 +35,29 @@ usd_label = Label(root, text='- USD').place(x=5, y=20)
 eur_label = Label(root, text='- EUR').place(x=5, y=40)
 rur_label = Label(root, text='- RUR').place(x=5, y=60)
 btc_label = Label(root, text='- BTC').place(x=5, y=80)
-enter_label = Label(root, text='Enter the name of the currency: ').place(x=5, y=100)
-buy_label = Label(root, text='Buy: ').place(x=5, y=140)
-sale_label = Label(root, text='Sale: ').place(x=5, y=170)
-grn_label_buy = Label(root, text='UAH').place(x=100, y=141)
-grn_label_sale = Label(root, text='UAH').place(x=100, y=171)
+enter_label = Label(root, text='Select currency: ').place(x=5, y=115)
+buy_label = Label(root, text='Buy: ').place(x=5, y=170)
+sale_label = Label(root, text='Sale: ').place(x=5, y=200)
+grn_label_buy = Label(root, text='UAH').place(x=100, y=171)
+grn_label_sale = Label(root, text='UAH').place(x=100, y=201)
 output_label_buy = Label(root, text='')
 output_label_buy.place(x=45, y=141)
 output_label_sale = Label(root, text='')
 output_label_sale.place(x=45, y=171)
-#main_edit = Entry(root, bg='lightblue', width=5)
-#main_edit.place(x=205, y=100)
-listbox1 = Listbox(root, height=5, width=15, selectmode=SINGLE)
-listbox1.place(x=150, y=150)
-listbox1.insert(1, 'USD')
-listbox1.insert(2, 'EUR')
-listbox1.insert(3, 'RUR')
-listbox1.insert(4, 'BTC')
 
 
-main_button = Button(root, text="Ok", command=handler).place(x=255, y=95)
+Listbox_currency = Listbox(root, height=2, width=15, selectmode=BROWSE)
+Listbox_currency.place(x=100, y=116)
+Listbox_currency.insert(1, 'USD')
+Listbox_currency.insert(2, 'EUR')
+Listbox_currency.insert(3, 'RUR')
+Listbox_currency.insert(4, 'BTC')
+
+scrollbar = Scrollbar(root, width=1)
+scrollbar.place(x=190, y=105)
+scrollbar['command'] = Listbox_currency.yview
+Listbox_currency['yscrollcommand'] = scrollbar.set
+
+main_button = Button(root, text="Ok", command=handler).place(x=215, y=110)
 root.mainloop()
 
